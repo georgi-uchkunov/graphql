b0fd06963bed948a629ae7a51346172fb4c06700
Admin <Admin@DESKTOP-G2QUO26>
2020-04-11 17:06:01 +0300

    First Commit

---
+++ src/graphql/types/Movie.js
@@ -0,0 +1,22 @@
+export default `
+
+    type Movie {
+        _id: String!
+        name: String!
+        description: String!
+        imageUrl: String!
+        price: Float!
+    }
+
+    type Query {
+        movie(_id: String!): Movie
+        movies: [Movie]
+    }
+
+    type Mutation {
+        addMovie(name: String!, description: String!, imageUrl: String!, price: Float!): Movie
+        deleteMovie(_id: String!): Movie
+        editMovie(_id: String!, name: String, description: String, imageUrl: String, price: Float): Movie
+    }
+
+`
